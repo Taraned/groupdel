18746aab00c2d2467b71f9c47dbd67cf58422517
Taraned <119848682+Taraned@users.noreply.github.com>
2022-12-13 12:09:25 +0100

    Update dogroupdel.sh

--- dogroupdel.sh
+++ dogroupdel.sh
@@ -1,7 +1,20 @@
 # Author: 
 
 function dogroupdel() {
-
-
+    ls
+    cd bin/
+    dialog --inputbox "Bitte Username eingeben:" Schnitzelwecken69
+    username -$(kdialog --inputbox "Bitte Username eingeben:") Schnitzelwecken69
+    dialog --inputbox
+    dialog --stdout--
+    echo $username
+    mkdir usermanager
+    cd usermanager
+    git init
+    ls
+    ls -ls
+    git status
+    nano dogroupdel.sh
+    groupdel "usermanager"
 }
 
